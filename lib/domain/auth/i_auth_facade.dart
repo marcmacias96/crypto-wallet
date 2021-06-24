a010d24334a6c588684ab7314482bfdab596196e
marcmacias96 <mamacias3@espe.edu.ec>
2021-06-23 21:25:51 -0500

    auth facade and failures

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,16 @@
+import 'package:crypto_wallet/domain/auth/value_objects.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'auth_failure.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
