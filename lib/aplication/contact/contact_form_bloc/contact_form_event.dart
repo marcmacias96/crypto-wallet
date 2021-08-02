1112df4b8af47a67672a0299ea787f504f7b8563
marcmacias96 <mamacias3@espe.edu.ec>
2021-08-01 21:04:54 -0500

    add event

--- lib/aplication/contact/contact_form_bloc/contact_form_event.dart
+++ lib/aplication/contact/contact_form_bloc/contact_form_event.dart
@@ -5,5 +5,7 @@ class ContactFormEvent with _$ContactFormEvent {
   const factory ContactFormEvent.initialized(
       String address, Option<Contact> contact) = Initialized;
   const factory ContactFormEvent.nameChanged(String name) = NameChanged;
+  const factory ContactFormEvent.addressChanged(String address) =
+      AddressChanged;
   const factory ContactFormEvent.save() = Save;
 }
