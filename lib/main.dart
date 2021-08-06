a03e73fb93924193f89844eb0b17168e1a838525
Odalis <odvalencia1@espe.edu.ec>
2021-08-05 22:28:59 -0500

    cambios contact

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:injectable/injectable.dart';
+import 'package:path_provider/path_provider.dart';
 import 'injection.dart';
 import 'my_bloc_observer.dart';
 import 'presentation/app_widget.dart';
+
 
 Future<void>  main() async {
   WidgetsFlutterBinding.ensureInitialized();
